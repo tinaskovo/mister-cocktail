9d7a202d50c0efa55dbfb1daf23be2f8b6c65ade
Tina Skov Olesen <tinaskovo@gmail.com>
2018-05-11 11:47:50 +0200

    trying again

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,4 +8,15 @@ environment.plugins.prepend('Provide',
     jQuery: 'jquery'
   })
 )
+
+const cssLoader = environment.loaders.get('css')
+const sassLoader = environment.loaders.get('sass')
+const postcssLoader = cssLoader.use.find(loader => loader.loader === 'postcss-loader')
+const postcssLoader2 = sassLoader.use.find(loader => loader.loader === 'postcss-loader')
+
+delete postcssLoader.options.config
+delete postcssLoader2.options.config
+
+
+
 module.exports = environment
