55e3fd7f55e650bb96de4907115bbaba73782aec
Tina Skov Olesen <tinaskovo@gmail.com>
2018-05-11 11:27:29 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'cocktails#index'
+  resources :cocktails, only: [:index, :new, :show, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
